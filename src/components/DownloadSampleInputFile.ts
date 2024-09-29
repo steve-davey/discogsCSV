75564b48504d7daf8891aa80cf82da40ce195b8e
steve-davey <double.happiness@outlook.com>
2024-09-29 15:19:49 +0100

    Various updates for publishing

--- src/components/DownloadSampleInputFile.ts
+++ src/components/DownloadSampleInputFile.ts
@@ -6,12 +6,23 @@ export default {
 }
 
 export function downloadSampleInputFile() {
-  const link = document.createElement('a')
-  link.href = '/src/assets/test_5_lines.csv'
-  link.target = '_blank'
-  link.download = 'test_5_lines.csv'
-
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
+  fetch('/test_5_lines.csv')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.blob();
+  })
+  .then(blob => {
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'test_5_lines.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: 'public', 
+  base: "/discogsCSV/",
   plugins: [
     vue(),
   ],
